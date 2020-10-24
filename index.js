4e4bfcc3a4fc3cf907ec1d06b69393f554715b85
maksimus <gorlovmaks23@gmail.com>
2020-10-24 10:11:56 +0600

    Добавленнны connections для places и items

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const categories = require("./app/category");
 const places = require("./app/places");
 const items = require("./app/items");
 const cors = require("cors");
-const db = require("./mysql");
 const mysql = require("mysql");
 const config = require("./config");
+const db = require("./mysql");
 
 const app = express();
 const port = 8000;
@@ -13,8 +13,6 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
-console.log(config.db)
 
 const connection = mysql.createConnection(config.db);
 
@@ -24,8 +22,8 @@ connection.connect((err) => {
         throw err;
     }
     app.use("/categories", categories(db(connection)));
-    app.use("/places", places);
-    app.use("/items", items);
+    app.use("/places", places(db(connection)));
+    app.use("/items", items(db(connection)));
     console.log("Connected to mysql");
     app.listen(port, () => {
         console.log(`Server started at http://localhost:${port}`);
